ccfb23bd0ae7145db54bd4c1079c3e0474ab505a
yarovikovanastya <43835909+yarovikovanastya@users.noreply.github.com>
2018-11-29 10:57:15 +0700

    Update Program.cs

--- fbi-lab-master/PrimeApp/Program.cs
+++ fbi-lab-master/PrimeApp/Program.cs
@@ -10,7 +10,7 @@ namespace PrimeApp
 
             var srv = new Prime.Services.PrimeService();
 
-            var prim = srv.PrimeName("savyar-test");
+            var prim = srv.PrimeName("savyar-octup");
 
             Console.WriteLine($"PrimeName: {prim}");
         }
